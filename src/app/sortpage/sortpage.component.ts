b76c05b23228e56312b6d2f71e3d485e23f3e162
wu-medoc <58807721+wu-medoc@users.noreply.github.com>
2020-10-26 10:58:25 +0800

    sortable update

--- src/app/sortpage/sortpage.component.ts
+++ src/app/sortpage/sortpage.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
 import { moreData, mySerivce } from '../interfaceData';
 import { Location } from '@angular/common';
-import { SortablejsOptions } from 'ngx-sortablejs/lib/sortablejs-options';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Component({
@@ -22,36 +21,7 @@ export class SortpageComponent implements OnInit, AfterViewInit {
   public noticeNine = false;
   /** 我的服務-加減class(remove-item:isAdd=false, add-item:isAdd=true) */
   isAdd = false;
-
-  // tslint:disable-next-line: deprecation
-  options: SortablejsOptions = {
-    disabled: true,
-    handle: '#myService',
-    draggable: '.mysvc',
-    group: '.mysvc',
-    onUnchoose: (evt) => {
-      // tslint:disable-next-line: radix
-      const id = parseInt(evt.item.id);
-      if (this.moreMy.length === 4){ return this.noticeFour = true ; }
-      const result = this.moreMy.findIndex(item => item.Function_ID === id);
-      if (this.moreMy.length > 4) {
-        if (result > -1) {
-          this.moreMy.splice(result, 1);
-          this.noticeFour = false ;
-          this.noticeNine = false ;
-          // 根據我的服務清單，修改下面更多服務的class狀態
-          this.groupCategary = this.svCategary.reduce((r, { Function_CategaryName: name, ...object }) => {
-            let temp = r.find(o => o.name === name);
-            if (!temp) { r.push(temp = { name, children: [] }); }
-            // tslint:disable-next-line: max-line-length
-            ((this.moreMy.filter( exclude => exclude.Function_ID === object.Function_ID)).length > 0) ? this.isAdd = true : this.isAdd = false;
-            temp.children.push({ object, isAdd: this.isAdd });
-            return r;
-          }, []);
-        }
-      }
-    },
-  };
+  pressed: any = {};
 
   /** Function_CategaryCode排序 */
   svCategary = moreData.sort((a, b) => a.Function_CategaryCode - b.Function_CategaryCode);
@@ -63,6 +33,12 @@ export class SortpageComponent implements OnInit, AfterViewInit {
     temp.children.push({ object, isAdd: this.isAdd });
     return r;
   }, []);
+
+  /** 拖曳觸發 */
+  logTouchstart(i: number) {
+    console.log(this);
+  }
+
   /** 更多服務按鈕增減 for #moreServiceList */
   serviceClick(code: number, action: boolean) {
     this.noticeNine = this.moreMy.length === 9 ? true : false;
@@ -104,17 +80,11 @@ export class SortpageComponent implements OnInit, AfterViewInit {
   /** editFunctionToggle */
   editFunctionToggle() {
     this.editFunction = true;
-    this.options = {
-      disabled: false,
-    };
   }
 
   /** 更新我的服務 */
   updateUserService(): void {
     this.editFunction = false;
-    this.options = {
-      disabled: true,
-    };
     // 如果我的服務沒有修改，不用save
     if ( JSON.stringify(this.moreOg) !== JSON.stringify(this.moreMy)) {
       this.moreOg.length = 0;
@@ -130,9 +100,6 @@ export class SortpageComponent implements OnInit, AfterViewInit {
 
   cancelUserService(): void {
     this.editFunction = false;
-    this.options = {
-      disabled: true,
-    };
   }
 
   // 返回鍵
