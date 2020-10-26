bc52d480fb469c1bc35d3fc306f641a178c0adc1
wu-medoc <58807721+wu-medoc@users.noreply.github.com>
2020-10-26 18:04:00 +0800

    sortablejs update

--- src/app/sortpage/sortpage.component.ts
+++ src/app/sortpage/sortpage.component.ts
@@ -20,61 +20,46 @@ export class SortpageComponent implements OnInit, AfterViewInit {
   public noticeFour = false;
   /** 我的服務-數量提醒最多9個 */
   public noticeNine = false;
+  public groupCategary = [];
   /** 我的服務-加減class(remove-item:isAdd=false, add-item:isAdd=true) */
   isAdd = false;
-  isClick = false;
-  pressed: any = {};
   // tslint:disable-next-line: deprecation
   options: SortablejsOptions = {
     disabled: true,
     handle: '#myService',
     draggable: '.mysvc',
-    group: '.mysvc'
+    group: '.mysvc',
+    onStart: (evt) => {
+      console.log('s', evt, evt.item.id);
+    },
+    onUnchoose: (evt) => {
+      console.log('c', evt.originalEvent.returnValue, evt.item.id);
+      evt.preventDefault();
+      if (evt.originalEvent.returnValue === true){
+        // tslint:disable-next-line: radix
+        this.serviceClick(parseInt(evt.item.id), false);
+      }
+    },
   };
 
   /** Function_CategaryCode排序 */
-  svCategary = moreData.sort((a, b) => a.Function_CategaryCode - b.Function_CategaryCode);
-  /** Function_CategaryName群組 */
-  groupCategary = this.svCategary.reduce((r, { Function_CategaryName: name, ...object }) => {
-    let temp = r.find(o => o.name === name);
-    if (!temp) { r.push(temp = { name, children: [] }); }
-    ((this.moreMy.filter( exclude => exclude.Function_ID === object.Function_ID)).length > 0) ? this.isAdd = true : this.isAdd = false;
-    temp.children.push({ object, isAdd: this.isAdd });
-    return r;
-  }, []);
+  sortClass() {
+    const svCategary = moreData.sort((a, b) => a.Function_CategaryCode - b.Function_CategaryCode);
+    /** Function_CategaryName群組 */
+    this.groupCategary = svCategary.reduce((r, { Function_CategaryName: name, ...object }) => {
+      let temp = r.find(o => o.name === name);
+      if (!temp) { r.push(temp = { name, children: [] }); }
+      ((this.moreMy.filter( exclude => exclude.Function_ID === object.Function_ID)).length > 0) ? this.isAdd = true : this.isAdd = false;
+      temp.children.push({ object, isAdd: this.isAdd });
+      return r;
+    }, []);
+  }
 
-  /** 拖曳觸發 */
-  sortItem($event: any, code: number) {
-    // console.log($event.target);
-    $event.preventDefault();
-    $event.stopPropagation();
-    if (this.editFunction === true){
-      $event.target.addEventListener('click', (ev) => {
-        this.options = {
-          disabled: true,
-        };
-        // console.log(ev);
-        this.serviceClick(code, false);
-        this.isClick = true;
-      });
-    }
-    if (this.isClick === true) {
-      this.options = {
-        disabled: true,
-      };
-    }else{
-      this.isClick = false;
-    }
-    $event.target.addEventListener('touchmove', (ev) => {
-      this.options = {
-        disabled: false,
-      };
-    });
-    $event.target.addEventListener('touchend', (ev) => {
-      this.options = {
-        disabled: true,
-      };
-    });
+  sortStart() {
+    this.options = {disabled: false};
+  }
+  sortEnd() {
+    this.options = {disabled: true};
   }
 
   /** 更多服務按鈕增減 for #moreServiceList */
@@ -87,24 +72,20 @@ export class SortpageComponent implements OnInit, AfterViewInit {
       // 已經在我的服務內且數量>4，就將這個ICON移出我的服務
       if (this.moreMy.length > 4) {
         this.moreMy.splice(result, 1);
+        this.sortClass();
         this.noticeNine = false;
       }
     } else {
       // 不在我的服務內且數量<9，就在我的服務增加這個ICON
       if (this.moreMy.length < 9) {
-      const add = this.moreList.filter(item => item.Function_ID === code)[0];
-      if (add !== undefined) { this.moreMy.push(add); }
-      this.noticeFour = false;
+        const add = this.moreList.filter(item => item.Function_ID === code)[0];
+        if (add !== undefined) {
+          this.moreMy.push(add);
+          this.sortClass();
+          this.noticeFour = false;
+        }
       }
     }
-    // 根據我的服務清單，修改下面更多服務的class狀態
-    this.groupCategary = this.svCategary.reduce((r, { Function_CategaryName: name, ...object }) => {
-      let temp = r.find(o => o.name === name);
-      if (!temp) { r.push(temp = { name, children: [] }); }
-      ((this.moreMy.filter( exclude => exclude.Function_ID === object.Function_ID)).length > 0) ? this.isAdd = true : this.isAdd = false;
-      temp.children.push({ object, isAdd: this.isAdd });
-      return r;
-    }, []);
   }
 
   constructor(private location: Location, private http: HttpClient, private elementRef: ElementRef) {
@@ -118,6 +99,7 @@ export class SortpageComponent implements OnInit, AfterViewInit {
   /** editFunctionToggle */
   editFunctionToggle() {
     this.editFunction = true;
+    this.sortStart();
   }
 
   /** 更新我的服務 */
@@ -132,12 +114,14 @@ export class SortpageComponent implements OnInit, AfterViewInit {
     }else{
       this.moreOg = mySerivce.concat();
     }
+    this.sortEnd();
   }
 
   /** 取消更新我的服務 */
 
   cancelUserService(): void {
     this.editFunction = false;
+    this.sortEnd();
   }
 
   // 返回鍵
@@ -146,6 +130,7 @@ export class SortpageComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
+    this.sortClass();
     // console.log(this.groupCategary);
   }
   ngAfterViewInit() {
